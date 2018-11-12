739607f8092fb6731710cc6d28dc9f2b80cd9d61
Joshua Biddick <joshua.biddick@cucumber.co.nz>
2018-11-12 17:01:59 +1300

    horrible addition of login form

---
+++ we-need-love/iNeedHelpDesperately.js
@@ -0,0 +1,61 @@
+document.prototype.frm = document.querySelector('.form');
+
+function Cancel() 
+{
+    document.frm.username.value="";
+    document.frm.password.value="";
+    document.getElementById("username").focus();
+}
+
+function checklogin(name, password) 
+{
+    var m_username = "admin123";
+    var m_password = "milan12345"
+
+    if(name!="admin123" || password!="milan12345") 
+        return false;
+    else
+        return true;
+}
+
+function Login() {
+    alert("You're logged in!");
+}
+
+function _submit()
+{
+    var f=document.frm;
+    var username=f.username.value;
+    var password=f.password.value;
+    if(f.username.value=="") 
+    {
+        alert("Username and Password cannot be empty");
+        f.username.focus();
+        return false;
+    }
+    if(false == checklogin(f.username.value, f.password.value)) 
+    {
+        alert("User and Password are incorrect");
+        document.frm.username.value="";
+        document.frm.password.value="";
+        document.getElementById("username").focus();
+        Cancel();
+    } else {
+
+    }
+}
+
+var submit_button = document.getElementsByTagName('button');
+
+if(typeof document.frm === 'object') 
+{
+    if(submit_button.length > 0) 
+    {
+        submit_button = submit_button[0]; 
+        //Submit button click
+        submit_button.addEventListener('click', function() {
+            _submit();
+        });
+    }
+}
+
